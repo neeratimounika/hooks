06a2a3d0ae5e1fb76fd86d0fae0890f225120aea
mounika.neerati <mounika.neerati93@gmail.com>
2024-02-07 12:25:41 +0530

    from useState_objct

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import './App.css';
 // import HooksCounter from './components/HooksCounter';
-import HooksCounter2 from './components/HooksCounter2';
+// import HooksCounter2 from './components/HooksCounter2';
+import HooksCounterObjct from './components/HooksCounterObjct';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       {/* <HooksCounter/> */}
-      <HooksCounter2/>
+      {/* <HooksCounter2/> */}
+      <HooksCounterObjct/>
     </div>
   );
 }
---
+++ src/components/HooksCounterObjct.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+
+const HooksCounterObjct = () => {
+    const [name,setName]=useState({firstName:'',lastName:''})
+
+
+
+  return (
+    <form>
+        <input type='text' value={name.firstName} onChange={e=>setName({...name, firstName:e.target.value})} />
+        <input type='text' value={name.lastName} onChange={e=>setName({...name, lastName:e.target.value})}/>
+        <h2>Youe first Name is -{name.firstName}</h2>
+        <h2>Youe Last Name is -{name.lastName}</h2>
+        <h2>{JSON.stringify(name)}</h2>
+    </form>
+  )
+}
+
+export default HooksCounterObjct

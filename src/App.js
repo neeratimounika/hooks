289ec87b4d9c689af04303591eee8b5e8733efe1
mounika.neerati <mounika.neerati93@gmail.com>
2024-02-07 12:42:58 +0530

    from useState_array

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import React from 'react';
 import './App.css';
 // import HooksCounter from './components/HooksCounter';
 // import HooksCounter2 from './components/HooksCounter2';
-import HooksCounterObjct from './components/HooksCounterObjct';
-
+// import HooksCounterObjct from './components/HooksCounterObjct';
+import UseStateArray from './components/UseStateArray';
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       {/* <HooksCounter/> */}
       {/* <HooksCounter2/> */}
-      <HooksCounterObjct/>
+      {/* <HooksCounterObjct/> */}
+      <UseStateArray/>
     </div>
   );
 }
